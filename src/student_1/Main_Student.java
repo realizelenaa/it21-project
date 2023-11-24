89e468a75c6d684e6c0e94a689276799dd266bc9
realizelenaa <elenarealisanx@gmail.com>
2023-11-24 19:27:53 +0800

    Added student_1

---
+++ src/student_1/Main_Student.java
@@ -0,0 +1,13 @@
+package student_1;
+
+class Main_Student {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Elena A. Realisan", "IT21C", "Do not do to others what you don't want others do unto you.");
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+    }
+}
