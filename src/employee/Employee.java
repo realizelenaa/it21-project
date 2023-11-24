c0991e52f1b474fcb6e3d7b3e5c63edd081285a0
realizelenaa <elenarealisanx@gmail.com>
2023-11-24 18:59:14 +0800

    Added employee

---
+++ src/employee/Employee.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+package employee;
+
+class Employee {    
+    private final String name;
+    private final double salary;
+    private final LocalDate hireDate;
+
+    public Employee(String name, double salary, LocalDate hireDate) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDate = hireDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+}
